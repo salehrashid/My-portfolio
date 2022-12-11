6f3f206bb3c23877e0f5c11583351433e1d88c6d
salehrashid <saleh.rashid.b@gmail.com>
2022-12-11 20:40:18 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    new Typed("#typed", {
+        strings: ["a Mobile developer", "and Web developer"],
+        typeSpeed: 70,
+        delaySpeed: 50,
+        loop: true
+    })
+})
+AOS.init();
